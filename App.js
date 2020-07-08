972ab8c036ee8ff744fdc3a140494b72d4cf5c8a
Kelviny H <kelvinyhenrique17@gmail.com>
2020-07-08 02:17:51 -0300

    Add app configs

--- App.js
+++ App.js
@@ -13,96 +13,146 @@ import {
   StatusBar,
   View,
   Animated,
-  Image
+  Image,
+  TouchableOpacity,
+  Text
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
- 
-} from 'react-native/Libraries/NewAppScreen';
 import { WebView } from 'react-native-webview';
 import AnimationBall from './src/components/AnimationBall';
+import AnimationBallIndicator from './src/components/AnimationBallIndicator';
 import bg from './assets/bg.png';
+import Logo from './assets/logo.svg';
+import Cart from './assets/cart.svg';
+import Back from './assets/back.svg';
+import Next from './assets/next.svg';
 
 
 const App: () => React$Node = () => {
 
   const [isLoaded, setLoaded] = useState('flex');
+  const [isLoading, setLoading] = useState(false);
+  const [loadFirst, setLoadFirst] = useState(true);
   const fadeAnim = useRef(new Animated.Value(0)).current;
+  const webviewRef = useRef(null)
+  const [canGoBack, setCanGoBack] = useState(false)
+  const [canGoForward, setCanGoForward] = useState(false)
+  const [currentUrl, setCurrentUrl] = useState('');
+  const [showTabBar, setShowTabBar] = useState(false);
+
+
   const fadeIn = () => {
 
     Animated.timing(fadeAnim, {
       toValue: 1,
       duration: 4000,
-      useNativeDriver:false
+      useNativeDriver: false
     }).start();
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     fadeIn();
   });
 
+  const handleEndLoading = () => {
+    setLoading(false);
+    setLoaded('none');
+    if(loadFirst == true){
+    setLoadFirst(false);
+    setShowTabBar(true);
+    }
+   
+  }
+
+  const handleStartLoad = () => {
+    if (loadFirst == false) {
+      setLoading(true)
+    }
+  }
+
+
+
+  const backButtonHandler = () => {
+    if (webviewRef.current) webviewRef.current.goBack()
+  }
+
+  const frontButtonHandler = () => {
+    if (webviewRef.current) webviewRef.current.mainDocumentURL()
+  }
 
   return (
     <>
-      <StatusBar barStyle="light-content"  backgroundColor="#353535"/>
-      <View style={{width:'100%', height:'100%', backgroundColor:'#000', display:isLoaded, alignItems:'center', justifyContent:'center'}}>
-      <Image source={bg} style={{width:"100%", height:'100%'}} />
-        
-      <AnimationBall/>
+      <StatusBar barStyle="light-content" backgroundColor="#353535" />
+      <View style={{ width: '100%', height: '100%', backgroundColor: '#000', display: isLoaded, alignItems: 'center', justifyContent: 'center' }}>
+        <Image source={bg} style={{ width: "100%", height: '100%' }} />
+
+        <AnimationBall />
+        <View style={{ width: '100%', height: '70%', position: 'absolute', alignSelf: 'center', justifySelf: 'center' }}>
+          <Logo style={{ width: '100%', height: '100%', alingSelf: '' }} />
+        </View>
       </View>
       <WebView
         source={{
-          uri: 'https://www.bsport.com.br'
+          uri: 'https://superirmao.loji.com.br/'
         }}
         style={{ marginTop: 20 }}
-        onLoadEnd={()=>setLoaded('flex')}
+        onLoadEnd={handleEndLoading}
+        onLoadProgress={handleStartLoad}
+
+        ref={webviewRef}
+        onNavigationStateChange={navState => {
+          setCanGoBack(navState.canGoBack)
+          setCanGoForward(navState.mainDocumentURL)
+          setCurrentUrl(navState.url)
+        }}
       />
+      {isLoading &&
+        <View style={{ width: '100%', height: '100%', alignItems: 'center', justifyContent: 'center', position: 'absolute' }}>
+          <AnimationBallIndicator />
+        </View>
+
+      }
+
+     {showTabBar && <View style={styles.tabBarContainer}>
+        <TouchableOpacity onPress={backButtonHandler}>
+          <Next width={30} height={30} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.ball}>
+          <Cart width={35} height={35} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={backButtonHandler}>
+          <Back width={30} />
+        </TouchableOpacity>
+      </View>}
     </>
   );
 };
+export default App;
+
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  flexContainer: {
+    flex: 1
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  tabBarContainer: {
+    padding: 15,
+    height: 50,
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    backgroundColor: '#FFF',
   },
-  body: {
-    backgroundColor: Colors.white,
+  button: {
+    color: '#000',
+    fontSize: 24
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+  ball: {
+    width: 60,
+    height: 60,
+    marginTop: -30,
+    backgroundColor: '#FF6F64',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+  }
+})
